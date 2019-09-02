602f5862d252382e6dcb6ecb1b79c6af50c5fbda
wolf <nyan643.ym25mm10@gmail.com>
2019-09-03 04:42:55 +0900

    fix: add other class method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { SheetService } from './sheet.service';
-
+import { FormService } from './form.service';
+import { Calculation } from './calculation';
+// main
 declare var global: any;
 
 global.createNewFile = (): void => {
-  SheetService.createSS('fawefawe');
-  // const ss = SheetService.createInitialFile('fwaefawejfia');
-  // ss.getRange('A4').setValue('Happy gas!');
-  // Logger.log(ss.getName());
+  const sheet = SheetService.getSheet(SSURL).getName();
+  const form = FormService.getForm(FURL).getResponses();
+  let formColumn = form[0].getItemResponses(); //カラムの情報が入っている。->配列
+
+  Calculation.getFormColumn(formColumn);
+  Calculation.getFormData(form);
+  Calculation.print();
 };
