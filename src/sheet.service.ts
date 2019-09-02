38e644a1b014ea6b59f7f8a616793314a8228f51
wolf <nyan643.ym25mm10@gmail.com>
2019-09-03 04:36:15 +0900

    fix: cut not using

--- src/sheet.service.ts
+++ src/sheet.service.ts
@@ -1,20 +1,10 @@
 import Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet;
-import drive = GoogleAppsScript.Drive.File;
-import { getDayFormat } from './util';
+// import { getDayFormat } from './util';
 
 export class SheetService {
-  static createInitialFile(prefix: string): Spreadsheet {
-    const fileName = `${prefix} ${getDayFormat()}`;
-    const ss = SpreadsheetApp.create(fileName);
+  static getSheet(prefix: string): Spreadsheet {
+    const url = `${prefix}`;
+    const ss = SpreadsheetApp.openByUrl(url);
     return ss;
-  }
-  static createSS(prefix: string): void {
-    let FOLDER_ID = '';
-    const fileName = `${prefix} ${getDayFormat()}`;
-    const ss = SpreadsheetApp.create(fileName).getId();
-    const fileSS = DriveApp.getFileById(ss);
-    const folder = DriveApp.getFolderById(FOLDER_ID);
-
-    folder.addFile(fileSS);
   }
 }
